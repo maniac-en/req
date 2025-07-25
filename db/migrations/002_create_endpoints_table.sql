e01534a1e676fc6a6594011dc69259b6eee43af9
Mudassir <mughalmudassir966@gmail.com>
2025-07-26 03:13:50 +0500

    added endpoints table

---
+++ db/migrations/002_create_endpoints_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+CREATE TABLE endpoints (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    collection_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    method TEXT NOT NULL,
+    url TEXT NOT NULL,
+    headers TEXT DEFAULT '{}' NOT NULL,
+    query_params TEXT DEFAULT '{}' NOT NULL,
+    request_body TEXT DEFAULT '' NOT NULL,
+    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (collection_id) REFERENCES collections(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE endpoints;
+
