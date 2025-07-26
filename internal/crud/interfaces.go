1a4498b9fd7cc894340afc3cd4b1fe39a61386e8
Shivam Mehta <sm.cse17@gmail.com>
2025-07-26 19:22:54 +0530

    feat(crud): add basic interfaces for crud package

---
+++ internal/crud/interfaces.go
@@ -0,0 +1,27 @@
+package crud
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+var (
+	ErrNotFound     = errors.New("entity not found")
+	ErrInvalidInput = errors.New("invalid input")
+)
+
+type Entity interface {
+	GetID() int64
+	GetName() string
+	GetCreatedAt() time.Time
+	GetUpdatedAt() time.Time
+}
+
+type Manager[T Entity] interface {
+	Create(ctx context.Context, name string) (T, error)
+	Read(ctx context.Context, id int64) (T, error)
+	Update(ctx context.Context, id int64, name string) (T, error)
+	Delete(ctx context.Context, id int64) error
+	List(ctx context.Context) ([]T, error)
+}
